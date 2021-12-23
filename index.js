481a952ce5e38e8612c5d09beb5a06d8f6d6843e
mayiseeyouagain <94270096+mayiseeyouagain@users.noreply.github.com>
2021-12-23 11:49:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3|欣怡|祝你|生日|快乐|happy|forever');
+      S.UI.simulate('|#countdown 3|JXY|祝你|生日|快乐|HAPPY|FOREVER');
     }
 
     S.Drawing.loop(function () {
